10f80c987540e17cf8dfb40737d99a3157d21a52
Azajul-Karim <azajulkarim2021@gmail.com>
2024-12-03 14:19:38 +0600

    first commit

---
+++ class_1/variable.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  int a = 10;
+  int b;
+  b = 20;
+  int c = 30, d = 40;
+
+  // printf("%d", a);
+  printf("a = %d, b = %d, c = %d, d = %d", a, b, c, d);
+}
+
+// Note
+// number = 32bit
+//  4byte = 32bit
+//  8byte = 64bit
