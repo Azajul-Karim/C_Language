ae4f101e8a76c58bffb8c5cc680365d87d855ec7
Azajul-Karim <azajulkarim2021@gmail.com>
2024-12-09 07:43:09 +0600

    new data type added

--- class_1/variable.c
+++ class_1/variable.c
@@ -7,16 +7,24 @@ int main()
   int c = 30, d = 40;
   int sum = a + b;
   int sub = a - b;
-  // printf("%d", a);
-  // printf("a = %d, b = %d, c = %d, d = %d", a, b, c, d);
-  // printf("a + b = %d\n", sum);
-  // printf("a - b = %d\n", sub);
+  printf("%d\n", a);
+  printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
+  printf("a + b = %d\n", sum);
+  printf("a - b = %d\n", sub);
 
   double PI = 3.14159;
   double n = 10.3653443156;
+  float f = 10.3653443156;
+  long long int number = 1000000000000000000;
+  unsigned int number2 = 1000000000;
+  unsigned long long int number3 = 1000000000000000000;
 
   printf("PI = %lf\n", PI);
   printf("n = %.7lf\n", n);
+  printf("f = %.3f\n", f);
+  printf("number = %lld\n", number);
+  printf("number2 = %u\n", number2);
+  printf("number3 = %llu\n", number3);
 }
 
 // Note
