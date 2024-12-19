1e6742bd36dec9b267eca4424bc04999838cb8e5
Azajul-Karim <azajulkarim2021@gmail.com>
2024-12-19 18:41:26 +0600

    new practice problem added

--- class_2/operator.c
+++ class_2/operator.c
@@ -2,16 +2,39 @@
 
 int main()
 {
-    int n;
-    scanf("%d", &n); // n = 123
+    // int n;
+    // scanf("%d", &n); // n = 123
 
-    int c = n % 10; // c = 1
-    n = n / 10;     // n = 23
+    // int c = n % 10; // c = 1
+    // n = n / 10;     // n = 23
 
-    int b = n % 10; // b = 3
-    n = n / 10;     // n = 2
+    // int b = n % 10; // b = 3
+    // n = n / 10;     // n = 2
 
-    int a = n % 10; // c =
+    // int a = n % 10; // c =
 
-    printf("a = %d, b = %d, c = %d\n", a, b, c);
+    // printf("a = %d, b = %d, c = %d\n", a, b, c);
+
+    // int A, B, C, D, X;
+
+    // scanf("% d % d % d % d", &A, &B, &C, &D);
+
+    // X = (A * B) - (C * D);
+
+    // printf("Difference  =  %d", X);
+
+    int x, y;
+
+    // Read input
+    // scanf("%d %d", &x, &y);
+
+    x = 5;
+    y = 10;
+
+    // Print results
+    printf("%d + %d = %d\n", x, y, x + y);
+    printf("%d * %d = %d\n", x, y, x * y);
+    printf("%d - %d = %d\n", x, y, x - y);
+
+    return 0;
 }
